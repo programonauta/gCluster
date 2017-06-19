5ae6f2e29e1f89bf7a2b7df0e96fa4ca3f4b96de
Ricardo Brandao <rbrandao.br@gmail.com>
2017-06-19 12:35:35 -0300

    Parsing phase ready

--- src/gluster.cpp
+++ src/gluster.cpp
@@ -26,6 +26,8 @@ int main(int argc, char* argv[])
   double dataVar = 0.;
   vector<string> lineCSV, headerCSV;
 
+  vector<double> maxValue, minValue;
+
   cout << "Grid Clustering algorithm" << endl;
   cout << "developed by ricardo brandao - https://github.com/programonauta/grid-clustering" << endl;
   cout << "===============================================================================\n" << endl;
@@ -43,44 +45,107 @@ int main(int argc, char* argv[])
 
   if (!getCSVLine(infile, headerCSV))
   {
-    cout << "Error on CSV Header" << endl;
+    cout << "Error reader CSV File (Header) on the first line" << endl;
     return 1;
   }
 
   dimension = headerCSV.size();
 
-  cout << "CSV File header read. Dimension: " << dimension << endl;
+  cout << "Reading CSV File header. Dimensions:" << dimension << endl;
 
-  Point sample(dimension);
+  for (int i = 0; i < headerCSV.size(); i++)
+    cout << (i?",": "") << headerCSV[i];
 
-  sample.coord[0] = 2.323;
-  sample.coord[1] = -5.332;
-  sample.coord[2] = 0.393;
+  cout << endl;
 
-  cout << "Point " << sample << endl;
+  if (!getCSVLine(infile, lineCSV))
+  {
+    cout << "Error reading CSV File (Header) on Max line" << endl;
+    return 1;
+  }
+
+  if (lineCSV.size() != dimension)  // Test if line of max values has the same dimension than header
+  {
+    cout << "Error reading CSV File (Header): Max line - dimension doesn't match. Read " << lineCSV.size() << " Expected: " << dimension << " dimensions"  << endl ; 
+    return 1;
+  }
+
+  maxValue.resize(dimension);
+  minValue.resize(dimension);
+
+  for (int i = 0; i < dimension; i++)
+  {
+    if (!isDouble(lineCSV[i].c_str()))
+    { 
+      cout << "Error reading CSV File (Header): Max line Field number " << i+1 << ", " << lineCSV[i] << " is not double" << endl;
+      return 1;
+    }
+    maxValue[i] = stod(lineCSV[i]);
+  }
+
+  cout << "CSV File - Max Line read - OK" << endl;
+  lineCSV.resize(0);
+  lineCSV.shrink_to_fit();
+
+  if (!getCSVLine(infile, lineCSV))
+  {
+    cout << "Error reading CSV File (Header) on Min line" << endl;
+    return 1;
+  }
+
+  if (lineCSV.size() != dimension)  // Test if line of min values has the same dimension than header
+  {
+    cout << "Error reading CSV File (Header): Min line - dimension doesn't match. Read " << lineCSV.size() << " Expected: " << dimension << " dimensions"  << endl ; 
+    return 1;
+  }
+
+  for (int i = 0; i < dimension; i++)
+  {
+    if (!isDouble(lineCSV[i].c_str()))
+    { 
+      cout << "Error reading CSV File (Header): Min line Field number " << i+1 << ", " << lineCSV[i] << " is not double" << endl;
+      return 1;
+    }
+    minValue[i] = stod(lineCSV[i]);
+
+    if (minValue[i] >= maxValue[i]) // Test if min value is greater or equal than max value
+    {
+      cout << "Error reading CSV File (Header): Min value gretaer or equal than Max value for dimension " << i+1 << ": " << minValue[i] << " isn't less than " << maxValue[i] << endl;
+      return 1;
+    }
+  }
+
+  cout << "CSV File - Min Line read - OK" << endl;
+  lineCSV.resize(0);
+  lineCSV.shrink_to_fit();
+
+  cout << "CSV File - Reading Data" << endl;
 
   while (infile)
   {
     r = getCSVLine(infile, lineCSV);
-    cout << "Line number: " << ++lineNo << " Number of fields: " << lineCSV.size() << "\n";
-    for (int i = 0; i < lineCSV.size(); i++)
-    {
-      cout << "   " << lineCSV[i] << " (is ";
-      if (!isDouble(lineCSV[i].c_str()))
-        cout << "not ";
-      cout << "double) ,";
-      if (isDouble(lineCSV[i].c_str())) cout << "|" << stod(lineCSV[i]) << "|, ";
-    }
-    cout << "\n";
+    ++lineNo;
     if (!r)
     {
-      cout << "Error - line " << lineNo << "\n\n";
-      return 0;
+      cout << "Error reading CSV File (Data) on Line " << lineNo << "\n";
+      return 1;
+    }
+    if (lineCSV.size() != dimension)
+    {
+      cout << "Error reading CSV File (Data) on Line " << lineNo << ": dimension doesn't match" << endl;
+      return 1;
+    }
+    for (int i = 0; i < lineCSV.size(); i++)
+    {
+      if (!isDouble(lineCSV[i].c_str()))
+      {
+        cout << "Error reading CSV File (Data) on Line: " << lineNo << ", Field: " << i+1 << " (" << lineCSV[i] << ") is not double" << endl;
+        return 1;
+      }
     }
     lineCSV.resize(0);
-    cout << "\n";
   }
 
-  cout << "Everything OK!\n**==**==**==**==**==**==**==\n\n";
+  cout << "CSV File - Total of " << lineNo << " lines of data read" << endl;
 
 }
